556da86d7af76013f6f932cc229acbaad0158319
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-23 12:03:13 -0600

    add mechanic interface definition

---
+++ src/interfaces/mechanic.interface.ts
@@ -0,0 +1,11 @@
+export interface Customer {
+  id: string;
+  dni: string;
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+  is_active: boolean;
+  created_at: Date;
+  updated_at: Date;
+}
