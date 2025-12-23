c45ba2a2f3618ba735a123a81caf8edd8d1b2047
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-23 12:02:52 -0600

    add vehicle interface definition

---
+++ src/interfaces/vehicle.interface.ts
@@ -0,0 +1,13 @@
+export interface Vehicle {
+  id: string;
+  plate: string;
+  brand: string;
+  model: string;
+  year: number;
+  color: string;
+  cubic_capacity: number;
+  customer_id: string;
+  is_active: boolean;
+  created_at: Date;
+  updated_at: Date;
+}
