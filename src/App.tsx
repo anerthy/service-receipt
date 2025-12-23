991385422b8ebd71892132cce1da11aab40d4155
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-23 15:17:54 -0600

    refactor: simplify App component by removing service fetching logic and rendering NewServiceReceipt

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,6 @@
-import { useState, useEffect } from 'react';
-import { supabase } from './lib/supabase';
-import type { Service } from './interfaces';
+import { NewServiceReceipt } from './pages/NewServiceReceipt';
 
 function App() {
-  const [services, setServices] = useState<Service[]>([]);
-
-  useEffect(() => {
-    async function getServices() {
-      const { data } = await supabase
-        .from('services')
-        .select()
-        .order('id', { ascending: true });
-
-      const services = data as Service[] | [];
-
-      if (services === null) return;
-
-      if (services.length > 1) {
-        setServices(services);
-      }
-    }
-
-    getServices();
-  }, []);
-
-  return (
-    <ul>
-      {services.map(({ id, name }) => (
-        <li key={id}>{name}</li>
-      ))}
-    </ul>
-  );
+  return <NewServiceReceipt />;
 }
 export default App;
