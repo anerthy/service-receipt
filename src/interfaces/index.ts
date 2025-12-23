28359945254ace94307ae14c8a4c4e35290413ea
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-23 12:03:20 -0600

    add index file for interface exports

---
+++ src/interfaces/index.ts
@@ -0,0 +1,6 @@
+export type { Customer } from './customer.interface';
+export type { Mechanic } from './mechanic.interface';
+export type { OrderDetail } from './order-detail.interface';
+export type { Service } from './service.interface';
+export type { ServiceOrder } from './service-order.interface';
+export type { Vehicle } from './vehicle.interface';
