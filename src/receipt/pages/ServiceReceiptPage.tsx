234fd7de936bf4dd7c698f5a8bc51a41d98239ba
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-28 14:50:18 -0600

    feat: replace material icons with lucide-react components in ServiceReceiptPage

--- src/receipt/pages/ServiceReceiptPage.tsx
+++ src/receipt/pages/ServiceReceiptPage.tsx
@@ -3,6 +3,8 @@
 import { Link } from 'react-router';
 import { ServiceReceiptList } from '../components/ServiceReceiptList';
 import { TopHeader } from '../components/TopHeader';
+import { Plus, Search, SlidersHorizontal } from 'lucide-react';
+import { Input } from '@/components/ui/input';
 
 export const ServiceReceiptPage = () => {
   return (
@@ -12,15 +14,18 @@ export const ServiceReceiptPage = () => {
         <label className="flex flex-col w-full">
           <div className="flex w-full items-center rounded-xl bg-white dark:bg-card-dark shadow-sm border border-slate-200 dark:border-slate-700 overflow-hidden h-12 transition-all focus-within:ring-2 focus-within:ring-primary/50 focus-within:border-primary">
             <div className="flex items-center justify-center pl-4 text-slate-400 dark:text-slate-500">
-              <span className="material-symbols-outlined">search</span>
+              <Search />
             </div>
-            <input
+            {/* <Input
               className="flex-1 w-full border-none bg-transparent px-3 py-2 text-slate-900 dark:text-white placeholder:text-slate-400 focus:ring-0 text-base"
               placeholder="Buscar cliente, placa..."
-              // value=""
+            /> */}
+            <Input
+              className="flex-1 w-full border-none bg-transparent px-3 py-2 text-slate-900 placeholder:text-slate-400 focus:ring-0 text-base"
+              placeholder="Buscar cliente, placa..."
             />
             <button className="pr-4 text-primary font-medium text-sm">
-              <span className="material-symbols-outlined">tune</span>
+              <SlidersHorizontal />
             </button>
           </div>
         </label>
@@ -32,8 +37,8 @@ export const ServiceReceiptPage = () => {
 
       <Link to={'/new-service-receipt'}>
         <div className="absolute bottom-6 right-6 z-30">
-          <button className="flex items-center justify-center h-14 w-14 rounded-2xl bg-primary text-white shadow-lg shadow-blue-500/40 hover:bg-blue-600 active:scale-95 transition-all">
-            <span className="material-symbols-outlined text-[28px]">add</span>
+          <button className="flex items-center justify-center h-14 w-14 rounded-2xl bg-primary text-white hover:bg-blue-600">
+            <Plus className="text-[28px]" />
           </button>
         </div>
       </Link>
