84269615d77122b793b58231eba3955b90f148d4
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-28 15:34:54 -0600

    feat: add @tanstack for data fetching and state management

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import { RouterProvider } from 'react-router';
 import { AppRouter } from './app.router';
 import { ThemeProvider } from '@/components/theme-provider';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <ThemeProvider>
-      <RouterProvider router={AppRouter} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={AppRouter} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
