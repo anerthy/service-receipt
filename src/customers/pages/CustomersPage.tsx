317e8f092247665a5c235f77b64e1cd24cbe4b79
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-28 20:46:28 -0600

    feat: implement CustomersPage with customer data fetching and display

--- src/customers/pages/CustomersPage.tsx
+++ src/customers/pages/CustomersPage.tsx
@@ -1,3 +1,67 @@
-export const CustomersPage = () => {
-  return <div>CustomersPage</div>;
-};
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { FullScreenLoading } from '@/components/custom/FullScreenLoading';
+import type { Customer } from '@/interfaces';
+import { Pen, Wrench } from 'lucide-react';
+import { Badge } from '@/components/ui/badge';
+import { useCustomers } from '../hooks/useCustomers';
+import { Pagination } from '@/components/custom/Pagination';
+
+export function CustomersPage() {
+  const { data, isLoading, error } = useCustomers();
+
+  if (isLoading) return <FullScreenLoading />;
+
+  if (error) return <div>Error loading customers.</div>;
+  const customers: Customer[] = data || [];
+
+  return (
+    <Table>
+      <TableCaption>Lista de clientes del Taller</TableCaption>
+      <TableHeader>
+        <TableHead className="flex gap-1 items-center justify-start">
+          <Wrench />
+          <h1 className="font-bold text-2xl">Servicios</h1>
+        </TableHead>
+        <TableRow>
+          <TableHead className="w-25">DNI</TableHead>
+          <TableHead>Nombre</TableHead>
+          <TableHead>Telefono</TableHead>
+          <TableHead>Email</TableHead>
+          <TableHead>Acciones</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {customers.map((customer) => (
+          <TableRow key={customer.id}>
+            <TableCell className="font-medium">{customer.dni}</TableCell>
+            <TableCell>{customer.name}</TableCell>
+            <TableCell>{customer.phone}</TableCell>
+            <TableCell>{customer.email}</TableCell>
+            <TableCell>
+              <Badge variant="secondary">
+                <Pen />
+                Editar
+              </Badge>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableCell colSpan={5}>
+            <Pagination totalPages={Math.ceil(customers.length / 10)} />
+          </TableCell>
+        </TableRow>
+      </TableFooter>
+    </Table>
+  );
+}
