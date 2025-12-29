9dbfda808ca0a3c73fceb35dc2ebfab4f8cb7bff
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-28 23:58:04 -0600

    feat: implement VehiclesPage to display vehicle data with loading state

--- src/vehicles/pages/VehiclesPage.tsx
+++ src/vehicles/pages/VehiclesPage.tsx
@@ -1,6 +1,12 @@
 import { AdminHeader } from '@/dasboard/components/AdminHeader';
+import { useVehicles } from '../hooks/useVehicles';
+import { FullScreenLoading } from '@/components/custom/FullScreenLoading';
 
 export const VehiclesPage = () => {
+  const { data: vehicles, isLoading } = useVehicles({});
+
+  if (isLoading) return <FullScreenLoading />;
+
   return (
     <div>
       <AdminHeader
@@ -8,6 +14,18 @@ export const VehiclesPage = () => {
         description="Gestión de vehículos"
         to="/dashboard/vehicles/new"
       />
+      {vehicles && vehicles.length === 0 ? (
+        <p>No hay vehículos registrados.</p>
+      ) : (
+        <ul>
+          {vehicles?.map((vehicle) => (
+            <li key={vehicle.id}>
+              {' '}
+              {vehicle.model} ({vehicle.year})
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
