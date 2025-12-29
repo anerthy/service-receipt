06d02d7e26aa8f5bce75b1ab729c27168786eb5d
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-28 19:00:46 -0600

    feat: enhance route protection with user authentication checks

--- src/components/routes/ProtectedRoutes.tsx
+++ src/components/routes/ProtectedRoutes.tsx
@@ -1,10 +1,28 @@
+import { useAuth } from '@/hooks/use-auth';
 import type { PropsWithChildren } from 'react';
+import { Navigate } from 'react-router';
 
 export const AuthenticatedRoute = ({ children }: PropsWithChildren) => {
+  const { user } = useAuth();
+
+  const status = user ? 'authenticated' : 'not-authenticated';
+
+  // if (status === 'checking') return null;
+
+  if (status === 'not-authenticated') return <Navigate to="/auth/login" />;
+
   return children;
 };
 
 export const NotAuthenticatedRoute = ({ children }: PropsWithChildren) => {
+  const { user } = useAuth();
+
+  const status = user ? 'authenticated' : 'not-authenticated';
+
+  // if (status === 'checking') return null;
+
+  if (status === 'authenticated') return <Navigate to="/" />;
+
   return children;
 };
 
