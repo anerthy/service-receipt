31c537bacdb8a47d7cb2f3804e0d4163499d0e90
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-28 22:55:55 -0600

    feat: implement useCustomer hook for managing customer data queries and mutations

---
+++ src/customers/hooks/useCustomer.tsx
@@ -0,0 +1,31 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { upsertCustomer } from '../actions/upsert-customer';
+import { getCustomerAction } from '../actions/get-customer.action';
+
+export const useCustomer = (id: string) => {
+  const queryClient = useQueryClient();
+
+  const query = useQuery({
+    queryKey: ['customers', { id }],
+    queryFn: async () => getCustomerAction(id),
+    staleTime: 1000 * 60 * 5, // 5 minutes
+    enabled: !!id,
+  });
+
+  const mutation = useMutation({
+    mutationFn: upsertCustomer,
+    onSuccess: (customer) => {
+      queryClient.invalidateQueries({ queryKey: ['customers'] });
+      queryClient.invalidateQueries({
+        queryKey: ['customers', { id: customer.id }],
+      });
+
+      queryClient.setQueryData(['customers', { id: customer.id }], customer);
+    },
+    onError: (error) => {
+      console.error('Error upserting customer:', error);
+    },
+  });
+
+  return { ...query, mutation };
+};
