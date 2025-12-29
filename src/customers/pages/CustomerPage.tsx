42a10ff3938ea57834049d8323c90cb517389b57
anerthy <112020687+anerthy@users.noreply.github.com>
2025-12-28 22:56:05 -0600

    feat: enhance CustomerPage with loading and error handling, and implement customer submission logic

--- src/customers/pages/CustomerPage.tsx
+++ src/customers/pages/CustomerPage.tsx
@@ -1,7 +1,58 @@
-import { useParams } from 'react-router';
+import type { Customer } from '@/interfaces';
+import { useNavigate, useParams } from 'react-router';
+import { CustomerForm } from './ui/CustomerForm';
+import { useCustomer } from '../hooks/useCustomer';
+import { FullScreenLoading } from '@/components/custom/FullScreenLoading';
+import { toast } from 'sonner';
 
 export const CustomerPage = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
+  const {
+    data: customer,
+    isLoading,
+    isError,
+    error,
+    mutation,
+  } = useCustomer(id!);
 
-  return <div>Customer no. {id}</div>;
+  if (isLoading) {
+    return <FullScreenLoading />;
+  }
+
+  if (isError) {
+    return <div>{error.message}</div>;
+  }
+
+  if (!customer) {
+    return <div>Error loading customer.</div>;
+  }
+
+  const handleSubmit = async (customerLike: Partial<Customer>) => {
+    console.log(customerLike);
+
+    await mutation.mutateAsync(customerLike, {
+      onSuccess: (customer) => {
+        toast.success('Datos del cliente guardados correctamente');
+        navigate(`/dashboard/customers/${customer.id}`);
+      },
+      onError: (error) => {
+        toast.error(
+          'Ha ocurrido un error al guardar los datos del cliente ' +
+            error.message,
+          {
+            position: 'top-right',
+          }
+        );
+      },
+    });
+  };
+
+  return (
+    <CustomerForm
+      customer={customer}
+      onSubmit={handleSubmit}
+      isPending={false}
+    />
+  );
 };
