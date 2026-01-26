e538e649f76e30b9b792addb792b2b99b1821ff8
anerthy <112020687+anerthy@users.noreply.github.com>
2026-01-25 23:52:14 -0600

    feat: display DasboardPage for authenticated users on LandingPage

--- src/components/custom/LandingPage.tsx
+++ src/components/custom/LandingPage.tsx
@@ -2,9 +2,15 @@ import { Link } from 'react-router';
 import { Button } from '../ui/button';
 import { useAuth } from '@/hooks/use-auth';
 import { signOut } from '@/auth/services/auth.services';
+import { DasboardPage } from '@/dasboard/pages/DasboardPage';
 
 export const LandingPage = () => {
   const { session } = useAuth();
+
+  if (session) {
+    return <DasboardPage />;
+  }
+
   return (
     <>
       <div className="flex flex-col justify-center items-center mt-20 mb-10">
