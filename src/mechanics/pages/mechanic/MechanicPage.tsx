78501ed352cfdcc0f756aa326a156a035cea79af
anerthy <112020687+anerthy@users.noreply.github.com>
2026-01-26 11:59:47 -0600

    refactor: simplify MechanicPage component by removing unused code

--- src/mechanics/pages/mechanic/MechanicPage.tsx
+++ src/mechanics/pages/mechanic/MechanicPage.tsx
@@ -1,56 +1,58 @@
-import type { Customer } from '@/interfaces';
-import { useNavigate, useParams } from 'react-router';
-import { FullScreenLoading } from '@/components/custom/FullScreenLoading';
-import { toast } from 'sonner';
+import { useParams } from 'react-router';
 
 export const MechanicPage = () => {
   const { id } = useParams();
-  const navigate = useNavigate();
-  const {
-    data: mechanic,
-    isLoading,
-    isError,
-    error,
-    mutation,
-  } = useMechanic(id!);
-
-  if (isLoading) {
-    return <FullScreenLoading />;
-  }
-
-  if (isError) {
-    return <div>{error.message}</div>;
-  }
-
-  if (!customer) {
-    return <div>Error loading customer.</div>;
-  }
-
-  const handleSubmit = async (customerLike: Partial<Customer>) => {
-    console.log(customerLike);
-
-    await mutation.mutateAsync(customerLike, {
-      onSuccess: (customer) => {
-        toast.success('Datos del cliente guardados correctamente');
-        navigate(`/dashboard/customers/${customer.id}`);
-      },
-      onError: (error) => {
-        toast.error(
-          'Ha ocurrido un error al guardar los datos del cliente ' +
-            error.message,
-          {
-            position: 'top-right',
-          },
-        );
-      },
-    });
-  };
-
-  return (
-    <CustomerForm
-      customer={customer}
-      onSubmit={handleSubmit}
-      isPending={false}
-    />
-  );
+  return <div>Mechanic Page {id}</div>;
 };
+
+// export const MechanicPage = () => {
+//   const { id } = useParams();
+//   const navigate = useNavigate();
+//   const {
+//     data: mechanic,
+//     isLoading,
+//     isError,
+//     error,
+//     mutation,
+//   } = useMechanic(id!);
+
+//   if (isLoading) {
+//     return <FullScreenLoading />;
+//   }
+
+//   if (isError) {
+//     return <div>{error.message}</div>;
+//   }
+
+//   if (!customer) {
+//     return <div>Error loading customer.</div>;
+//   }
+
+//   const handleSubmit = async (customerLike: Partial<Customer>) => {
+//     console.log(customerLike);
+
+//     await mutation.mutateAsync(customerLike, {
+//       onSuccess: (customer) => {
+//         toast.success('Datos del cliente guardados correctamente');
+//         navigate(`/dashboard/customers/${customer.id}`);
+//       },
+//       onError: (error) => {
+//         toast.error(
+//           'Ha ocurrido un error al guardar los datos del cliente ' +
+//             error.message,
+//           {
+//             position: 'top-right',
+//           },
+//         );
+//       },
+//     });
+//   };
+
+//   return (
+//     <CustomerForm
+//       customer={customer}
+//       onSubmit={handleSubmit}
+//       isPending={false}
+//     />
+//   );
+// };
